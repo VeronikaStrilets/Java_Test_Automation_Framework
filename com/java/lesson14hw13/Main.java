959f2fb751a6d4f98c0369f128ce3ac9705ef1ba
VeronikaStrilets <veronika.strilets@gmail.com>
2024-03-29 12:47:13 +0100

    Added HW13

---
+++ com/java/lesson14hw13/Main.java
@@ -0,0 +1,21 @@
+package lesson14hw13;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String text = "Arise, fair sun, and kill the envious moon.";
+
+        Map<String, Integer> wordCountMap = Words.countWords(text);
+
+        Map<String, Integer> sortedByLengthMap = new TreeMap<>(new Sorting());
+        sortedByLengthMap.putAll(wordCountMap);
+
+        System.out.println("Words and their occurrence (sorted by length):");
+        for (Map.Entry<String, Integer> entry : sortedByLengthMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
